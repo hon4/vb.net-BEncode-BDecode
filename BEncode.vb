8ad1d926f10a7230665bc6b77ea0343cf8ab9800
hon <38807226+hon4@users.noreply.github.com>
2024-09-03 14:44:19 +0300

    Update BEncode.vb
    Encodings enhancement. Direct byte add. Faster encode.

--- BEncode.vb
+++ BEncode.vb
@@ -2,7 +2,7 @@
 '| BEncode Class by hon |
 '#======================#
 '| Coded by: hon        |
-'| Version: 2.0.1       |
+'| Version: 2.2.0       |
 '| Date: D07-M08-Y2024  |
 '| honcode.blogspot.com |
 '+----------------------+
@@ -14,55 +14,63 @@ Public Class BEncode
     End Function
 
     Private Shared Function BEncode_str(ByVal s As String) As Byte()
-        Return str2bytez(s.Length & ":" & s)
+        Return BEncode_strBARR(Encoding.UTF8.GetBytes(s)) 'All str in UTF8
     End Function
 
     Private Shared Function BEncode_int(ByVal i As Int64) As Byte()
-        Return str2bytez("i" & i & "e")
+        Dim r As New List(Of Byte)
+        r.Add(105) '105=i
+        r.AddRange(Encoding.ASCII.GetBytes(i))
+        r.Add(101) '101=e
+        Return r.ToArray
     End Function
 
     Private Shared Function BEncode_dict(ByVal d As Dictionary(Of String, Object)) As Byte()
-        Dim ret As Byte() = str2bytez("d")
+        Dim ret As New List(Of Byte)
+        ret.Add(100) '100=d
         For Each x As KeyValuePair(Of String, Object) In d
-            ret = ret.Concat(BEncode_str(x.Key)).ToArray
+            ret.AddRange(BEncode_str(x.Key))
             If TypeOf x.Value Is String Then
-                ret = ret.Concat(BEncode_str(x.Value)).ToArray
+                ret.AddRange(BEncode_str(x.Value))
             ElseIf TypeOf x.Value Is Dictionary(Of String, Object) Then
-                ret = ret.Concat(BEncode_dict(x.Value)).ToArray
+                ret.AddRange(BEncode_dict(x.Value))
             ElseIf TypeOf x.Value Is List(Of Object) Then
-                ret = ret.Concat(BEncode_list(x.Value)).ToArray
+                ret.AddRange(BEncode_list(x.Value))
             ElseIf TypeOf x.Value Is Byte() Then
-                ret = ret.Concat(BEncode_strBARR(x.Value)).ToArray
+                ret.AddRange(BEncode_strBARR(x.Value))
             Else
-                ret = ret.Concat(BEncode_int(x.Value)).ToArray
+                ret.AddRange(BEncode_int(x.Value))
             End If
         Next
-        Return ret.Concat(str2bytez("e")).ToArray
+        ret.Add(101) '101=e
+        Return ret.ToArray
     End Function
 
     Private Shared Function BEncode_list(ByVal l As List(Of Object)) As Byte()
-        Dim ret As Byte() = str2bytez("l")
+        Dim ret As New List(Of Byte)
+        ret.Add(108) '108=l
         For Each x As Object In l
             If TypeOf x Is String Then
-                ret = ret.Concat(BEncode_str(x)).ToArray
+                ret.AddRange(BEncode_str(x))
             ElseIf TypeOf x Is Dictionary(Of String, Object) Then
-                ret = ret.Concat(BEncode_dict(x)).ToArray
+                ret.AddRange(BEncode_dict(x))
             ElseIf TypeOf x Is List(Of Object) Then
-                ret = ret.Concat(BEncode_list(x)).ToArray
+                ret.AddRange(BEncode_list(x))
             ElseIf TypeOf x Is Byte() Then
-                ret = ret.Concat(BEncode_strBARR(x)).ToArray
+                ret.AddRange(BEncode_strBARR(x))
             Else
-                ret = ret.Concat(BEncode_int(x)).ToArray
+                ret.AddRange(BEncode_int(x))
             End If
         Next
-        Return ret.Concat(str2bytez("e")).ToArray
-    End Function
-
-    Private Shared Function str2bytez(ByVal s As String) As Byte()
-        Return Encoding.Default.GetBytes(s)
+        ret.Add(101) '101=e
+        Return ret.ToArray
     End Function
 
     Private Shared Function BEncode_strBARR(ByVal b As Byte()) As Byte()
-        Return str2bytez(b.Length & ":").Concat(b).ToArray
+        Dim ret As New List(Of Byte)
+        ret.AddRange(Encoding.ASCII.GetBytes(b.Length))
+        ret.Add(58) '58=:
+        ret.AddRange(b)
+        Return ret.ToArray
     End Function
 End Class
