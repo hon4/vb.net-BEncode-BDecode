d51b055468712f0db5baf8511af4a0dbea2600e6
hon <38807226+hon4@users.noreply.github.com>
2024-09-03 14:41:53 +0300

    Update BDecode.vb
    Encodings enhancement. Direct byte cmp. Faster decode.

--- BDecode.vb
+++ BDecode.vb
@@ -2,7 +2,7 @@
 '| BDecode Class by hon |
 '#======================#
 '| Coded by: hon        |
-'| Version: 2.0.1       |
+'| Version: 2.2.0       |
 '| Date: D07-M08-Y2024  |
 '| honcode.blogspot.com |
 '+----------------------+
@@ -17,17 +17,17 @@ Public Class BDecode
     Private Shared Function BDecode_dict(ByVal b As Byte(), ByRef i As UInteger) As Dictionary(Of String, Object)
         Dim ret As New Dictionary(Of String, Object)
         i += 1 'skp 'd'
-        While Not bytez2str({b(i)}) = "e"
-            Dim key As String = bytez2str(BDecode_str(b, i))
-                If IsNumeric(bytez2str({b(i)})) Then
-                    ret(key) = BDecode_str(b, i)
-                ElseIf b(i) = str2bytez("i")(0) Then
-                    ret(key) = BDecode_int(b, i)
-                ElseIf b(i) = str2bytez("d")(0) Then
-                    ret(key) = BDecode_dict(b, i)
-                ElseIf b(i) = str2bytez("l")(0) Then
-                    ret(key) = BDecode_list(b, i)
-                End If
+        While Not b(i) = 101 '101=e
+            Dim key As String = Encoding.UTF8.GetString(BDecode_str(b, i))
+            If IsNumeric(Encoding.ASCII.GetString({b(i)})) Then
+                ret(key) = BDecode_str(b, i)
+            ElseIf b(i) = 105 Then '105=i
+                ret(key) = BDecode_int(b, i)
+            ElseIf b(i) = 100 Then '100=d
+                ret(key) = BDecode_dict(b, i)
+            ElseIf b(i) = 108 Then '108=l
+                ret(key) = BDecode_list(b, i)
+            End If
             'i += 1
         End While
         i += 1 'skp 'e'
@@ -36,50 +36,47 @@ Public Class BDecode
 
     Private Shared Function BDecode_str(ByVal b As Byte(), ByRef i As UInteger) As Byte()
         Dim lenstr As String = ""
-        While IsNumeric(bytez2str({b(i)}))
-            lenstr &= bytez2str({b(i)})
+        While IsNumeric(Encoding.ASCII.GetString({b(i)}))
+            lenstr &= Encoding.ASCII.GetString({b(i)})
             i += 1
         End While
         i += 1 'skp ':'
         Dim len As UInteger = CUInt(lenstr)
-        Dim ret As Byte() = b.Skip(i).Take(len).ToArray
-        i += len
-        Return ret
+        Dim ret As New List(Of Byte)
+        Dim ito As UInteger = i + len
+        While i < ito
+            ret.Add(b(i))
+            i += 1
+        End While
+        Return ret.ToArray
     End Function
 
     Private Shared Function BDecode_int(ByVal b As Byte(), ByRef i As UInteger) As Int64
         i += 1 'skp 'i'
-        Dim retbytes As Byte() = {}
-        While Not bytez2str({b(i)}) = "e"
-            retbytes = retbytes.Concat({b(i)}).ToArray
+        Dim retbytes As New List(Of Byte)
+        While Not b(i) = 101 '101=e
+            retbytes.Add(b(i))
             i += 1
         End While
         i += 1 'skp 'e'
-        Return CType(bytez2str(retbytes), Int64)
+        Return CType(Encoding.ASCII.GetString(retbytes.ToArray), Int64)
     End Function
 
     Private Shared Function BDecode_list(ByVal b As Byte(), ByRef i As UInteger) As List(Of Object)
         Dim ret As New List(Of Object)
         i += 1 'skp 'l'
-        While Not bytez2str({b(i)}) = "e"
-            If IsNumeric(bytez2str({b(i)})) Then
+        While Not b(i) = 101 '101=e
+            If IsNumeric(Encoding.ASCII.GetString({b(i)})) Then
                 ret.Add(BDecode_str(b, i))
-            ElseIf b(i) = str2bytez("i")(0) Then
+            ElseIf b(i) = 105 Then '105=i
                 ret.Add(BDecode_int(b, i))
-            ElseIf b(i) = str2bytez("d")(0) Then
+            ElseIf b(i) = 100 Then '100=d
                 ret.Add(BDecode_dict(b, i))
-            ElseIf b(i) = str2bytez("l")(0) Then
+            ElseIf b(i) = 108 Then '108=l
                 ret.Add(BDecode_list(b, i))
             End If
         End While
         i += 1 'skp 'e'
         Return ret
-    End Function
-
-    Private Shared Function str2bytez(ByVal s As String) As Byte()
-        Return Encoding.Default.GetBytes(s)
-    End Function
-    Private Shared Function bytez2str(ByVal b As Byte()) As String
-        Return Encoding.Default.GetString(b)
     End Function
 End Class
